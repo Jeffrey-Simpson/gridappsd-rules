b763ab839011c3d10f2c74b6d9b9b762b0ce50b1
jsimpson <jsimpson@nrel.gov>
2018-06-20 13:37:36 -0600

    Fixed typos. Changed directory nam. Added import statement to __init__.py

---
+++ gridappsd_rules/__init__.py
@@ -0,0 +1,1 @@
+from . import gridappsd_rules
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@
 # -------------------------------------------------------------------------------
 from setuptools import setup, find_packages
 
-__version__ = '1.0'
+__version__ = '0.3'
 
 packages = find_packages('.')
 
